971e6a0ac1b9913d82bea68d6a3ea0260557a1b0
Samuray360 <151565282+Samuray360@users.noreply.github.com>
2025-02-16 00:47:08 -0400

    change 3

--- Type_the_word.py
+++ Type_the_word.py
@@ -2,31 +2,34 @@ import random
 import flet as ft
 repeted = False 
 user_word = ft.TextField(label="write the word")
-stored_words = [
+while repeted == False:
+        
+       
+    stored_words = [
     "Lantern", "Glacier", "Serendipity", "Tapestry", "Nebula", "Wanderlust", 
     "Harmonize", "Ember", "Cascade", "Quixotic", "Vortex", "Jubilant", 
     "Monolith", "Obsidian", "Reverie", "Echo", "Prism", "Mirage", "Enigma", 
     "Saffron", "Wistful", "Crescent", "Twilight", "Solitude", "Whisper", 
     "Aurora", "Labyrinth", "Ephemeral", "Euphoria", "Odyssey"]
-
-used_words=[]
-
-while repeted == False:
+    used_words=[]
     word = random.choice(stored_words)
+    word_display = ft.Text(f"Selected Word: {word}")
+
     if word in used_words: 
-        repeted == True
+            repeted == True
 
 def main(page: ft.Page):
-
 
     page.bgcolor = "white"
     page.window.height = 600
     page.window.width = 600
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
-# def word_comparetor():
-
-
+    page.add( word_display,user_word)
     
-    
+ft.app(main)   
+# def check_button():
+#     if word_display ==user_word :
+        # text.visibility_turn_on match
+
+        # else:
+         
+        # text.visiblitity_ turn_on dont match
