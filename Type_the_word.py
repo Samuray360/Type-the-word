7b08905d42300b24091ebde0268eab795739a0a1
Samuray360 <151565282+Samuray360@users.noreply.github.com>
2025-02-16 22:50:43 -0400

    change4

--- Type_the_word.py
+++ Type_the_word.py
@@ -1,35 +1,42 @@
 import random
 import flet as ft
 repeted = False 
-user_word = ft.TextField(label="write the word")
-while repeted == False:
-        
-       
-    stored_words = [
+user_word = ft.TextField(label="write the word" ,color="white")
+match_label = ft.Text("Match",color="green",visible=False)
+dont_match_label=ft.Text("Don't match",color="red",visible=False)
+  
+stored_words = [
     "Lantern", "Glacier", "Serendipity", "Tapestry", "Nebula", "Wanderlust", 
     "Harmonize", "Ember", "Cascade", "Quixotic", "Vortex", "Jubilant", 
     "Monolith", "Obsidian", "Reverie", "Echo", "Prism", "Mirage", "Enigma", 
     "Saffron", "Wistful", "Crescent", "Twilight", "Solitude", "Whisper", 
     "Aurora", "Labyrinth", "Ephemeral", "Euphoria", "Odyssey"]
-    used_words=[]
-    word = random.choice(stored_words)
-    word_display = ft.Text(f"Selected Word: {word}")
-
+used_words=[]
+word = random.choice(stored_words)
+while repeted == True:   
+    
     if word in used_words: 
-            repeted == True
+            repeted == False    
+    else:
+         repeted=True
 
+def check_button_clicked():
+    if word_display==user_word :
+        match_label.visible(True)
+    if word_display !=user_word :
+        dont_match_label.visible(True)
+    else:
+         match_label.visible(False)
+         dont_match_label.visible(False)
+
+word_display = ft.Text(f"Selected Word: {word}")
 def main(page: ft.Page):
 
-    page.bgcolor = "white"
+    page.bgcolor = "navy"
     page.window.height = 600
     page.window.width = 600
-    page.add( word_display,user_word)
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.add( word_display,user_word,ft.ElevatedButton( check_button_clicked(), text="check", color="white", width=200))
     
 ft.app(main)   
-# def check_button():
-#     if word_display ==user_word :
-        # text.visibility_turn_on match
-
-        # else:
-         
-        # text.visiblitity_ turn_on dont match
