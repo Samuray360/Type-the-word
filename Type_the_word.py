a821466202c9c0345e76ecc1888d71c4b3120353
Samuray360 <151565282+Samuray360@users.noreply.github.com>
2025-02-17 11:00:55 -0400

    change6

--- Type_the_word.py
+++ Type_the_word.py
@@ -3,7 +3,7 @@ import flet as ft
 repeted = False 
 user_word = ft.TextField(label="write the word" ,color="white")
 match_label = ft.Text("Match",color="green",visible=False)
-dont_match_label=ft.Text("Don't match",color="red",visible=False)
+missmatch_label=ft.Text("Don't match",color="red",visible=False)
   
 stored_words = [
     "Lantern", "Glacier", "Serendipity", "Tapestry", "Nebula", "Wanderlust", 
@@ -16,19 +16,26 @@ word = random.choice(stored_words)
 while repeted == True:   
     match_label = visible=False
     dont_match_label= visible=False
+
     if word in used_words: 
             repeted == False    
     else:
          repeted=True
 
 def check_button_clicked():
-    if word_display==user_word :
-        match_label.visible=True
-    if word_display !=user_word :
-        dont_match_label.visible=True
+   for i in range(len(word_display.value)):  
+    if user_word.value[i] == word_display.value[i]:
+        match_label.visible = True  
     else:
-         match_label.visible=False
-         dont_match_label.visible=False
+        missmatch_label.visible = True 
+
+        # if word_display==user_word :
+        #     match_label.visible=True
+                
+      
+    # else:
+    #      match_label.visible=False
+    #      dont_match_label.visible=False
 
 word_display = ft.Text(f"Selected Word: {word}")
 def main(page: ft.Page):
@@ -38,7 +45,7 @@ def main(page: ft.Page):
     page.window.width = 600
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.add( word_display,user_word,ft.ElevatedButton(text="check", color="white", width=200, on_click=check_button_clicked))
+    page.add( word_display,user_word,ft.ElevatedButton(text="check", color="white", width=200, on_click=check_button_clicked()))
 
     
 ft.app(main)   
