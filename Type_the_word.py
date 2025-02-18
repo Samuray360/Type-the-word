743528c347fb7ffd8e53dd86af9cf99e35563ad7
Samuray360 <151565282+Samuray360@users.noreply.github.com>
2025-02-17 22:44:53 -0400

    change7

--- Type_the_word.py
+++ Type_the_word.py
@@ -2,9 +2,10 @@ import random
 import flet as ft
 repeted = False 
 user_word = ft.TextField(label="write the word" ,color="white")
-match_label = ft.Text("Match",color="green",visible=False)
-missmatch_label=ft.Text("Don't match",color="red",visible=False)
-  
+match_label = ft.Text("Match",color="green",height=200,visible=False)
+missmatch_label=ft.Text("Don't match",color="red",height=200,visible=False)
+match_label=visible=False
+missmatch_label=visible=False
 stored_words = [
     "Lantern", "Glacier", "Serendipity", "Tapestry", "Nebula", "Wanderlust", 
     "Harmonize", "Ember", "Cascade", "Quixotic", "Vortex", "Jubilant", 
@@ -12,30 +13,20 @@ stored_words = [
     "Saffron", "Wistful", "Crescent", "Twilight", "Solitude", "Whisper", 
     "Aurora", "Labyrinth", "Ephemeral", "Euphoria", "Odyssey"]
 used_words=[]
-word = random.choice(stored_words)
-while repeted == True:   
-    match_label = visible=False
-    dont_match_label= visible=False
+word = random.choice([w for w in stored_words if w not in used_words])
+used_words.append(word)
 
-    if word in used_words: 
-            repeted == False    
-    else:
-         repeted=True
+def check_button_clicked(e):
+    match_label.visible = False
+    missmatch_label.visible = False
 
-def check_button_clicked():
-   for i in range(len(word_display.value)):  
-    if user_word.value[i] == word_display.value[i]:
+    if user_word.value == word_display.value:
+
         match_label.visible = True  
     else:
-        missmatch_label.visible = True 
+        missmatch_label.visible= True 
+        
 
-        # if word_display==user_word :
-        #     match_label.visible=True
-                
-      
-    # else:
-    #      match_label.visible=False
-    #      dont_match_label.visible=False
 
 word_display = ft.Text(f"Selected Word: {word}")
 def main(page: ft.Page):
@@ -45,7 +36,7 @@ def main(page: ft.Page):
     page.window.width = 600
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.add( word_display,user_word,ft.ElevatedButton(text="check", color="white", width=200, on_click=check_button_clicked()))
+    page.add( word_display,user_word,missmatch_label,match_label,ft.ElevatedButton(text="check", color="white", width=200, on_click=check_button_clicked))
 
     
 ft.app(main)   
